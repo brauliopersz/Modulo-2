a27fb4931905265779c169bd8886ecbad95904f8
Braulio <brauliopersz@gmail.com>
2025-05-07 21:36:29 -0400

    Se definen las funciones del módulo conversor_tiempo

---
+++ conversor_tiempo/timeConversion.py
@@ -0,0 +1,17 @@
+def hourToMinute_Convert():
+    pass
+
+def hourToSecond_Convert():
+    pass
+
+def minuteToHour_Convert():
+    pass
+
+def minuteToSecond_Convert():
+    pass
+
+def secondToHour_Convert():
+    pass
+
+def secondToMinute_Convert():
+    pass
