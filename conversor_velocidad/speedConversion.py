053b45074839c841f74a61723e306cbfcc56f672
Braulio <brauliopersz@gmail.com>
2025-05-18 22:27:34 -0400

    Se completa módulo de conversión

---
+++ conversor_velocidad/speedConversion.py
@@ -0,0 +1,30 @@
+
+def KmToMs(speed):
+    metro = 0
+    metro = speed / 3.59999997
+    return metro
+
+def KmToMph(speed):
+    milla = 0
+    milla = speed / 5793.74275782
+    return milla
+
+def MsToKm(speed):
+    kilometro = 0
+    kilometro = speed * 3.6
+    return kilometro
+
+def MsToMph(speed):
+    milla = 0
+    milla = speed * 2.23694
+    return milla
+
+def MphToKm(speed):
+    kilometro = 0
+    kilometro = speed * 1.60934133
+    return kilometro
+
+def MphToMs(speed):
+    metro = 0
+    metro = speed / 2.23693999
+    return metro
